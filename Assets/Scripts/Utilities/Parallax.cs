89b43bbbabac15c7a69e786db363f70ed1fb8d2a
neerwright <neerwright@gmail.com>
2022-05-10 19:11:03 +0200

    Project Setup and parallax script added

---
+++ Assets/Scripts/Utilities/Parallax.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+    [SerializeField]
+    Transform Player;
+
+    Vector2 startPosition;
+    float startZ;
+    float distanceFromSubject => transform.position.z - Player.position.z;
+    float clippingPlane => (cam.transform.position.z + (distanceFromSubject > 0? cam.farClipPlane: cam.nearClipPlane));
+
+    Vector2 newPos;
+    Vector2 travel => (Vector2)cam.transform.position - startPosition;
+    float parallaxVector => Mathf.Abs(distanceFromSubject / clippingPlane);
+    
+    
+    public void Start()
+    {
+        startPosition = transform.position;
+        startZ = transform.position.z;
+    }
+
+    public void Update() {
+        newPos = startPosition + travel* parallaxVector;
+        transform.position = new Vector3(newPos.x, newPos.y, startZ);
+    }
+}
