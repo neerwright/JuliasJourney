ed169b25d2f31baff0e0e3b211ef358e20132ed3
Rene <rene.plattner@student.uni-tuebingen.de>
2023-01-26 21:28:46 +0100

    Slope condition and state for statemachine

---
+++ Assets/Scripts/Player/Conditions/IsOnSlopeConditionSO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "State Machine/Conditions/Is Character Controller on a Slope")]
+public class IsOnSlopeConditionSO : StateConditionSO<IsOnSlopeCondition> { }
+
+public class IsOnSlopeCondition : Condition
+{
+	private PlayerController _playerController;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_playerController = stateMachine.GetComponent<PlayerController>();
+	}
+
+	protected override bool Statement() => _playerController.IsOnSlope;
+}
