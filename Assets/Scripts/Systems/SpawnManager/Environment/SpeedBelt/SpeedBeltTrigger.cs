b5dd7d69254d82e929af8dfeb420a4b9544e54aa
Rene <rene.plattner@student.uni-tuebingen.de>
2023-07-23 11:48:24 +0200

    messing with box

--- Assets/Scripts/Systems/SpawnManager/Environment/SpeedBelt/SpeedBeltTrigger.cs
+++ Assets/Scripts/Systems/SpawnManager/Environment/SpeedBelt/SpeedBeltTrigger.cs
@@ -9,14 +9,27 @@ namespace environment
     public class SpeedBeltTrigger : MonoBehaviour
     {
         public SpeedBelt _speedBelt;
+        public float _speedForPushBox = 10000;
+
+        private const string PLAYER_TAG = "Player";
+        private const string PUSHBOX_TAG = "PushBox";
 
         public void OnTriggerStay2D(Collider2D Collider)
                 {
-                    
-                    if(Collider.gameObject.tag == "Player")
+                    print("trig");
+                    if(Collider.gameObject.tag == PLAYER_TAG)
                     {    
                         _speedBelt.SpeedUpPlayer();
                         
+                    }
+
+                    if(Collider.gameObject.tag == PUSHBOX_TAG)
+                    {
+                        
+                        var rb = Collider.GetComponent<Rigidbody2D>();
+                        print(rb);
+                        if(rb)
+                            rb.AddForce(Vector2.right * Time.deltaTime * _speedForPushBox);
                     }
                 }
     }
