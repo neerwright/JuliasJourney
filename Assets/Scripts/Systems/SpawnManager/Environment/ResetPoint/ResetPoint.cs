547486240a6e8cdc1025ed1c2fed6364efa740e0
Rene <rene.plattner@student.uni-tuebingen.de>
2023-07-24 20:06:10 +0200

    torch added

--- Assets/Scripts/Systems/SpawnManager/Environment/ResetPoint/ResetPoint.cs
+++ Assets/Scripts/Systems/SpawnManager/Environment/ResetPoint/ResetPoint.cs
@@ -8,11 +8,16 @@ namespace environment
     public class ResetPoint : MonoBehaviour
     {
         [SerializeField] private Vec2EventSO _resetLocationEvent;
+        [SerializeField] private GameObject _firePS;
         
         public void OnTriggerEnter2D(Collider2D collider)
         {
             if(collider.gameObject.tag == "Player")
+            {
+                _firePS.SetActive(true);
                 _resetLocationEvent.Raise((Vector2) gameObject.transform.position);
+            }
+                
         }
 
 
