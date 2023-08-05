f2c86c81be11e4be01f5548bc29e51b9c65c0cf7
Rene <rene.plattner@student.uni-tuebingen.de>
2023-08-05 15:53:05 +0200

    reverted water back to sprite renderer

--- Assets/Scripts/Systems/SpawnManager/Environment/Water/Water.cs
+++ Assets/Scripts/Systems/SpawnManager/Environment/Water/Water.cs
@@ -4,72 +4,57 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
-    public float speed;
+    public float scrollSpeed;
 
-    [SerializeField] private Renderer waterRenderer;
+    private SpriteRenderer waterRenderer;
 
-    [SerializeField] private Material[] waterMaterial;
+    //[SerializeField] private Material waterMaterial;
+    [SerializeField] private Sprite[] waterTextures;
 
     private float _timer = 0f;
     private int _index = 0;
     private const float START_ANIMATION_THREASHOLD = 1.2f;
-    private const float SPEED = 0.2f;
+    private const float CHANGE_TEX_SPEED = 0.9f;
     private float _animationTimer = 0f;
     private bool forward = true;
 
     void Start()
     {
-        
+        waterRenderer = GetComponent<SpriteRenderer>();
+        InvokeRepeating("UpdateWater", 0, CHANGE_TEX_SPEED);
     }
 
 
-    void Update()
+    void UpdateWater()
     {
-        if(waterRenderer != null)
+        //waterRenderer.material.mainTextureOffset += new Vector2(scrollSpeed , 0f);
+        
+        if(forward)
         {
-            waterRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0f);
-
-            _timer += Time.deltaTime;
-
-            if(_timer > START_ANIMATION_THREASHOLD)
+            
+            _index++;
+            waterRenderer.sprite = waterTextures[_index];
+            if(_index >= waterTextures.Length -1)
             {
-                _animationTimer += Time.deltaTime;
-                
-                if(_animationTimer > SPEED)
-                {
-                    _animationTimer = 0;
+                _index = waterTextures.Length -1;
+                forward = false;
+            }
+        }
+        else
+        {
+            
+            
+            
+            _index--;
+            waterRenderer.sprite = waterTextures[_index];
 
-                    if(forward)
-                    {
-                        waterRenderer.material = waterMaterial[_index];
-                        _index++;
-                        if(_index >= waterMaterial.Length)
-                        {
-                            _index = waterMaterial.Length -1;
-                            forward = false;
-                        }
-                    }
-                    else
-                    {
-                        waterRenderer.material = waterMaterial[_index];
-                        _index--;
-
-                        if(_index > 0)
-                        {
-                            _index = 0;
-                            forward = true;
-                            _timer = 0;
-                        }
-                    }
-                }
+            if(_index <= 0)
+            {
+                _index = 0;
+                forward = true;
                 
             }
-            
-        
-
         }
-            
-
-            
+                       
     }
 }
